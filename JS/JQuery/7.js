1ac223a7e67ac417d5ec7cd3749b083d889ae0f1
songkg7 <56438906+songkg7@users.noreply.github.com>
2020-12-16 23:49:35 +0900

    fix event object

--- JS/JQuery/7.js
+++ JS/JQuery/7.js
@@ -5,14 +5,13 @@ $(() => {
   // NOTE: Replace hover() with on()
   // - CSS로 처리하는 것이 더 좋은 방법임.
   $('a')
-    .on('mouseover', function () {
+    .on('mouseover', function (event) {
       let word = $(this).text();
       word = $.trim(word);
       // word = word.trim();
       $(`.${word}`).show();
-      // FIXME: event 를 대체하기
-      const x = event.clientX;
-      const y = event.clientY;
+      const x = event.pageX;
+      const y = event.pageY;
       $(`.${word}`).css({ top: y, left: x });
     })
     .on('mouseout', function () {
