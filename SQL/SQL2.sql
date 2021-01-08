ca0f95dd69b92f5c39f8346762c4defdca321a10
songkg7 <56438906+songkg7@users.noreply.github.com>
2021-01-08 19:05:45 +0900

    add CRUD(C)

--- SQL/SQL2.sql
+++ SQL/SQL2.sql
@@ -1281,8 +1281,7 @@ WHERE emp_no =
       (SELECT emp_no
        FROM (SELECT emp_no,
                     rank,
-                    (SELECT COUNT(*) FROM employee e2 WHERE e1.rank = e2.rank) AS cnt,
-                    salary
+                    (SELECT COUNT(*) FROM employee e2 WHERE e1.rank = e2.rank) AS cnt
              FROM employee e1
              ORDER BY cnt DESC, salary DESC)
        WHERE rownum = 1);
@@ -1307,5 +1306,299 @@ SELECT *
 FROM employee
 ORDER BY salary DESC;
 
-SELECT * FROM employee_vw1;
+SELECT *
+FROM employee_vw1;
 
+CREATE OR REPLACE VIEW employee_vw1 AS
+SELECT emp_no,
+       emp_name,
+       dep_no,
+       rank,
+       hire_date,
+       resist_num,
+       phone_num,
+       mgr_emp_no
+FROM employee;
+
+SELECT *
+FROM employee_vw1;
+
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- Q.143 뷰 employee_vw1 에 데이터 '이승엽', 40, '과장', '1990-09-01', '7811231452719', '01090056376', 1 을 입력하면?
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+INSERT INTO employee_vw1 (emp_no, emp_name, dep_no, rank, hire_date, resist_num, phone_num,
+                          mgr_emp_no)
+VALUES ((SELECT NVL(MAX(emp_no), 0) + 1 FROM employee_vw1), '이승엽', 40, '과장',
+        TO_DATE('1990-09-01', 'YYYY-MM-DD'), '7811231452719', '01090056376', 1);
+SELECT *
+FROM employee_vw1
+ORDER BY emp_no DESC; -- 뷰를 검색
+SELECT *
+FROM employee
+ORDER BY emp_no DESC; -- employee 테이블 검색
+COMMIT;
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- Q.144 뷰 employee_vw1 에서 주민번호 '7811231452719', 직원명 '이승엽' 의 직급을 부장으로 수정하면?
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+UPDATE employee_vw1
+SET rank = '부장'
+WHERE emp_name = '이승엽'
+  AND resist_num = '7811231452719';
+
+SELECT *
+FROM employee_vw1
+ORDER BY emp_no DESC; -- 뷰를 검색
+SELECT *
+FROM employee
+ORDER BY emp_no DESC;
+-- employee 테이블 검색
+
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- Q.145 뷰 employee_vw1 에서 주민번호 '7811231452719', 직원명 '이승엽' 인 직원을 제거하면?
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+DELETE
+FROM employee_vw1
+WHERE resist_num = '7811231452719';
+
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- Q.146 [직원번호], [직원명], [직급], [부서번호], [부서명] 을 출력하는 뷰 employee_vw2를 생성하면?
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+CREATE VIEW employee_vw2 AS
+SELECT e.emp_no,
+       e.emp_name,
+       e.rank,
+       e.dep_no,
+       d.dep_name
+FROM dept d,
+     employee e
+WHERE d.dep_no = e.dep_no;
+
+SELECT *
+FROM employee_vw2;
+
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- Q.147 부서별, 직급별로 [부서번호], [부서명], [직급], [평균연봉] 을 출력하는 뷰 employee_vw3을 생성하면?
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+CREATE VIEW employee_vw3 AS
+SELECT e.dep_no             "dep_no"
+     , d.dep_name           "dep_name"
+     , e.rank               "rank"
+     , ROUND(AVG(e.salary)) "avg_salary"
+FROM employee e,
+     dept d
+WHERE d.dep_no = e.dep_no
+GROUP BY d.dep_name, e.dep_no, e.rank
+ORDER BY 1, 4 DESC;
+
+SELECT *
+FROM employee_vw3;
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- Q.148 다음 뷰에 대한 질문에 대답하면?
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+CREATE VIEW employee_vw4 AS
+SELECT emp_no, emp_name, dep_no
+FROM employee;
+---------------------------------------------------------------
+-- <문> 아래의 코딩은 성공하나 실패하나?
+INSERT INTO employee_vw4
+VALUES (30, '사오순', 40);
+-- ▶︎ 실패한다. 뷰 employee_vw4 에 입력되는 데이터는 employee 테이블에 입력되는 것과 같다.
+-- employee 테이블에서 not null, unique 등의 제약조건에 위배되기 때문에 못들어 간다.
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- Q.149 다음 뷰에 대한 질문에 대답하면?
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+INSERT INTO dept
+VALUES (60, '전략부', '부산');
+----------------------------------------------------------
+CREATE VIEW dept_vw1 AS
+SELECT dep_no, dep_name, loc
+FROM dept
+WHERE dep_no = 60
+WITH CHECK OPTION;
+-- where 조건을 위배하는 것은 보이지 않겠다.
+----------------------------------------------------------
+--<문> 아래의 코딩을 실행하면?
+UPDATE dept_vw1
+SET dep_no=70
+WHERE dep_no = 60;
+-- ▶︎ 실패한다. WITH CHECK OPTION 이 있으면 where 조건에 위배되는 입력, 수정은 불가능하다.
+-- ▶︎ 즉 dept_vw1 란 이름의 view 의 목적은 60번 부서를 검색하는게 목적이므로 이 목적을 위배하는 행위는 하지마라.
+--   위배하면 60번 부서를 검색하지 못하게 되니까 위배하지 마라의 의미.
+----------------------------------------------------------
+--<문> 아래의 코딩을 실행하면?
+INSERT INTO dept_vw1
+VALUES (70, '미래부', '대구');
+-- 을 실행하면?
+-- ▶︎ 실패한다. WITH CHECK OPTION 이 있으면 where 조건에 위배되는 입력, 수정은 불가능하다.
+-- ▶︎ 70번 부서 입력해봤자 dept_vw1이란 이름의 view 를 통해 볼 수 없으므로 이런 짓 하지마라.
+----------------------------------------------------------
+--<문> 아래의 코딩을 실행하면?
+DELETE
+FROM dept_vw1
+WHERE dep_no = 60;
+-- ▶︎ 성공. WITH CHECK OPTION 이 있으면 where 조건에 위배되는 입력, 수정만 불가능하다. 삭제는 적용되지 않는다.
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- Q.150 다음 뷰에 대한 질문에 대답하면?
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+----------------------------------------------------------
+CREATE VIEW employee_vw6 AS
+SELECT emp_no, emp_name, dep_no, rank, salary
+FROM employee
+WHERE salary >= 3000
+WITH CHECK OPTION;
+----------------------------------------------------------
+--<문> 아래의 코딩을 실행하면?
+UPDATE employee_vw6
+SET salary=salary * 0.95
+WHERE salary >= 3000;
+-- ▶︎ 실패한다. WITH CHECK OPTION 이 있으면 where 조건에 위배되는 입력, 수정은 불가능하다.
+-- ▶︎ 수정후 3000만원 밑으로 내려가면 또 where 조건을 위배되서 검색이 안되므로 안됨
+----------------------------------------------------------
+--<문> 아래의 코딩을 실행하면?
+UPDATE employee_vw6
+SET salary=salary * 1.95
+WHERE salary >= 3000;
+-- ▶︎ 성공. 업데이트로 3000 미만으로 내려가지 않아 조건에 위배되지 않으므로 성공한다.
+
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- 게시판 분석과 SQL 구문
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- Q.151 게시판이란?
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- 글쓴이의 주장, 의견 등의 의사표현을 웹상에서 구현한 것을 말한다.
+-- 예>
+--              [게시판 글 목록]           [6개 검색] [새글쓰기]
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+--  번호              제목             이름         작성일       조회수
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+--   6      청년 실직 문제는 대기업 문제   설현       2011.12.06      0
+----------------------------------------------------------------
+--   5      경제 침체의 원인            사오정      2011.12.01      2
+----------------------------------------------------------------
+--   4          ㄴ 안보문제            하하       2011.12.03      1
+----------------------------------------------------------------
+--   3            ㄴ 북한 핵 문제       전지현      2011.12.05     0
+----------------------------------------------------------------
+--   2          ㄴ 나라 빚 관리 부실     저팔계      2011.12.02      1
+----------------------------------------------------------------
+--   1            ㄴ 엔화 하락 문제     김태희      2011.12.04      0
+----------------------------------------------------------------
+
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- Q.152 게시판 관련 테이블을 만들면?
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+CREATE TABLE board
+(
+
+    b_no        number                    --글번호
+    ,
+    subject     varchar2(50)   NOT NULL   --글 제목
+    ,
+    wirter      varchar2(30)   NOT NULL   --글쓴이명
+    ,
+    reg_date    date      DEFAULT SYSDATE --등록일
+    ,
+    readcount   number(5) DEFAULT 0       --조회수
+    -----------------------------------------------------------------------------
+    ,
+    content     varchar2(2000) NOT NULL   --글 내용
+    ,
+    pwd         varchar2(12)   NOT NULL   --암호
+    ,
+    email       varchar2(30)              --이메일
+    -----------------------------------------------------------------------------
+    ,
+    group_no    number(5)      NOT NULL   --입력 글의 소속 그룹번호
+    ,
+    print_no    number(5)      NOT NULL   --같은 그룹 내에 출력 순서 번호
+    ,
+    print_level number(5)      NOT NULL   --같은 그룹 내에 댓글 레벨 단계
+    -----------------------------------------------------------------------------
+    ,
+    PRIMARY KEY (b_no)
+);
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- Q.153 게시판에 처음으로 사오정이 아래와 같은 글을 올린다면 이때 실행되는 SQL구문 모두 쓰면?
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+--제목 : 경제 침체의 원인
+--이름 : 사오정
+--내용 : 어쩌구 저쩌구
+--암호 : 1234
+--이메일 : a@naver.com
+
+--insert into board (???) values (???)
+-------------------------------------------------------------
+INSERT INTO board (b_no, subject, wirter, content, pwd, email, group_no, print_no, print_level)
+VALUES ( (SELECT NVL(MAX(b_no), 0) + 1 FROM board) --글번호
+       , '경제 침체의 원인' --글제목
+       , '사오정' --글쓴이
+           -------------------------------------------------
+       , '어쩌구 저쩌구'--글내용
+       , '1234'--암호
+       , 'a@naver.com'--이메일
+           -------------------------------------------------
+       , (SELECT NVL(MAX(b_no), 0) + 1 FROM board) --입력 글의 소속 그룹번호
+       , 0 --같은 그룹 내에 출력 순서번호
+       , 0 --같은 그룹 내에 댓글 레벨단계 번호
+
+       );
+
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+-- Q.154 저팔계가 사오정의 댓글로 아래와 같은 글을 올린다면 이때 실행되는 SQL구문 모두 쓰면?
+--       <단, 사오정의 PK,즉 글번호가 1이다>
+--mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+--제목 : 나라 빚 관리 부실
+--이름 : 저팔계
+--내용 : 어쩌구 저쩌구 내용물
+--암호 : 2345
+--이메일 : b@naver.com
+--------------------------------------------------------------
+--1. 엄마 조회수 1 증가
+--------------------------------------------------------------
+UPDATE board
+SET readcount = readcount + 1
+WHERE b_no = 1;
+--------------------------------------------------------------
+--2. 엄마의 후손 글들의 출력번호를 1씩 증가시키기
+--   그래야 내가 엄마 바로 밑으로 들어갈 수 있다.
+--------------------------------------------------------------
+UPDATE board
+SET readcount = readcount + 1
+WHERE b_no = 1;
+--------------------------------------------------------------
+--3.
+--------------------------------------------------------------
+INSERT INTO board (b_no, subject, wirter, content, pwd, email, group_no, print_no, print_level)
+VALUES ( (SELECT NVL(MAX(b_no), 0) + 1 FROM board)
+       , '나라 빚 관리 부실'
+       , '저팔계'
+           ---------------------
+       , '어쩌구 저쩌구 내용물'
+       , '2345'
+       , 'b@naver.com'
+           ---------------------
+       , (SELECT group_no FROM board WHERE b_no = 1) --엄마글의 그룹 번호를 검색하는 select 가 들어가는 쿼리
+       , (SELECT print_no + 1 FROM board WHERE b_no = 1) --엄마글의 출력순서 번호를 검색한 후 1 더하는 select 가 들어가는 쿼리
+       , (SELECT print_level + 1 FROM board WHERE b_no = 1) --엄마글의 댓글레벨 단계를 검색한 후 1 더하는 select 가 들어가는 쿼리
+
+       )
