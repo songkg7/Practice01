520b1301e051f9f2ea84629ade69cdebd78b2a71
songkg7 <56438906+songkg7@users.noreply.github.com>
2021-01-28 18:36:20 +0900

    project jpashop, add member

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    // JPA 스펙 상 생성한 생성자, 가급적 new 로 생성하지 말 것
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
