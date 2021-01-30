5d5de4abedbcad863fb759b3c5d471537d30b1de
songkg7 <56438906+songkg7@users.noreply.github.com>
2021-01-30 23:45:19 +0900

    add order service

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,5 +27,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직 (Setter 를 대체하기 위해서 사용하는 이유도 있다)
+
+    /*
+    stock 증가
+    */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /*
+    stock 감소
+    */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity-quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
